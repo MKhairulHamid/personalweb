6921c3d9ff54ea37de0c5919e09f5c948e5c685f
hamid <mkhairulhamid@gmail.com>
2025-01-19 11:00:23 +0700

    fix package

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/personalweb/', // Add the repository name here
+  base: '/personalweb/',
   plugins: [react()],
 });
